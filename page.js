af03793fdebdd317a76d0f05d64013327bf3e210
Teewa56 <ogunodemarvellous@gmail.com>
2024-11-09 16:48:53 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,18 @@
+document.getElementById("menu-button").onclick=()=>{
+    document.getElementById("side-nav").style.width = "100%";
+}
+document.getElementById("close").onclick=()=>{
+    document.getElementById("side-nav").style.width = "0";
+}
+document.getElementById("side-nav-id").onclick=()=>{
+    document.getElementById("side-nav").style.width ="0";
+}
+document.getElementById("side-nav-id2").onclick=()=>{
+    document.getElementById("side-nav").style.width ="0";
+}
+document.getElementById("side-nav-id3").onclick=()=>{
+    document.getElementById("side-nav").style.width ="0";
+}
+document.getElementById("side-nav-id4").onclick=()=>{
+    document.getElementById("side-nav").style.width ="0";
+}
